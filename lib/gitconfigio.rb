03df1bfbee0550b1df44a4197c54b17b89157841
mocchi <boom.boom.planet@gmail.com>
2015-02-20 20:34:44 +0900

    ADD: parse_node

--- lib/gitconfigio.rb
+++ lib/gitconfigio.rb
@@ -7,4 +7,21 @@ module GitConfigIO
     return key.gsub(/\s/,''),value.sub(/^\s/,'') if key && value
   end
 
+  def self.parse_node(line_source)
+    config = {}
+    head = nil
+    line_source.each do |l|
+      if /^\[(.+)\]$/ === l
+        head = {}
+        config[l.match(/^\[(.+)\]$/)[1]] = head
+      elsif /^#/ === l
+        next
+      elsif !!head
+        key,value = parse_value(l)
+        head[key] = value if key
+      end
+    end
+    config
+  end
+
 end
