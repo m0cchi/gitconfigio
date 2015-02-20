e868aa901233d2f6832fc8a48ead036f10b6fcb1
mocchi <boom.boom.planet@gmail.com>
2015-02-20 20:32:29 +0900

    ADD: parse_value

--- lib/gitconfigio.rb
+++ lib/gitconfigio.rb
@@ -2,4 +2,9 @@ require_relative "gitconfigio/version"
 
 module GitConfigIO
 
+  def self.parse_value(source)
+    key,value = source.split('=')
+    return key.gsub(/\s/,''),value.sub(/^\s/,'') if key && value
+  end
+
 end
